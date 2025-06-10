25bcc209dac2f5a963c477829961405500011b85
Regiane Melo <regianemelo1995@gmail.com>
2025-06-10 14:26:21 -0300

    global styles fora da funcao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+
 globalStyles()
 
+import logoImg from '../assets/logo.svg'
+
+import Image from "next/image"
+
+import { Container, Header } from "../styles/pages/app"
+
 function Myapp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default Myapp
+export default Myapp 
