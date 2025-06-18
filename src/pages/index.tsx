178864d0dcaac66cd72e2a5b7753ebc4226a5710
Regiane Melo <regianemelo1995@gmail.com>
2025-06-18 15:48:51 -0300

    utilizando getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 
 import { useKeenSlider } from 'keen-slider/react';
@@ -46,7 +46,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -65,6 +65,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, //a cada 2 horas, gera novamente a página home
   }
 }
